3a705b3f148a7c6cb3fa8981aaffbf05d87f8c09
Beingswhowereborn <dolmin0708@naver.com>
2024-07-15 16:38:22 +0900

    이미지 경로 확인 및 제품 소개 부분 변경

--- js/default.js
+++ js/default.js
@@ -1,117 +1,111 @@
-console.log("hello")
+console.log("hello");
 
 /**
- * 배경 이미지 변경 함수 
+ * 배경 이미지 변경 함수
  * --------------------------
  * 1. 이미지 배열에 src값 담기
  * 2. index 순서 세는 count 정하기
  * 3. src 변경하는 함수 생성
  * 4. setTimeOut으로 3초마다 실행시키기
- * 
+ *
  */
 
 const images = [
-  '../img/main__image-0.jpg',
-  '../img/main__image-1.jpg',
-  '../img/main__image-2.jpg'
+  "../img/main__image-0.jpg",
+  "../img/main__image-1.jpg",
+  "../img/main__image-2.jpg",
 ];
-
 
 let currentIndex = 1;
 
-
-$(document).ready(function(){
-  $('.main__hero').css("background", `url(./img/main__image-0.jpg)`); 
-
+$(document).ready(function () {
+  $(".main__hero").css("background", `url(./img/main__image-0.jpg)`);
 
   function changeBackgroundImage() {
-    $('.main__hero').css("background", `url(img/main__image-${currentIndex}.jpg)`); 
-    $('.main__hero').css("background-size", "cover"); 
+    $(".main__hero").css(
+      "background",
+      `url(img/main__image-${currentIndex}.jpg)`
+    );
+    $(".main__hero").css("background-size", "cover");
     currentIndex = (currentIndex + 1) % images.length;
 
     if (currentIndex == 3) {
-      currentIndex = 0
+      currentIndex = 0;
     }
   }
 
-setInterval(changeBackgroundImage, 3000);
-
-})
+  setInterval(changeBackgroundImage, 3000);
+});
 
 /**
- * tabmenu 
+ * tabmenu
  * --------------------------
  * 1. mouseEnter시 header padding-bottom 늘려서 sub-menu 배경 흰색 생성
  * 2. transition 0.3초 후 submenu display block
- * 3. mouseLeave시 기존 padding 값으로 변경, 배경색 투명으로 변경 
- * 
+ * 3. mouseLeave시 기존 padding 값으로 변경, 배경색 투명으로 변경
+ *
  */
 
-
-
-$(document).ready(function(){
+$(document).ready(function () {
   // $('.sub__menu').css({
   //   "display": "none",
   //   "opacity": 0
   // })
 
-  $('.gnb__item').mouseenter(function() {
-    $('.header__wrap').css({
+  $(".gnb__item").mouseenter(function () {
+    $(".header__wrap").css({
       "padding-bottom": "360px",
-      "background-color": "#fff"
+      "background-color": "#fff",
     });
-    $('.gnb__menu .gnb__item > a').css("color", "#000");
-    $('.logo img').attr('src', "img/main__logo.png")
+    $(".gnb__menu .gnb__item > a").css("color", "#000");
+    $(".logo img").attr("src", "../img/main__logo.svg");
 
     // 높이 애니메이션이 끝난 후 서브메뉴를 보이게 함
     setTimeout(() => {
-      $('.sub__menu').css({
-        "display": "block",
-        "opacity": 1
+      $(".sub__menu").css({
+        display: "block",
+        opacity: 1,
       });
     }, 300);
 
-    $('.sub__menu li a').css("display", "block");
+    $(".sub__menu li a").css("display", "block");
   });
 
-  $('.header__wrap').mouseleave(function() {
-    $('.header__wrap').css({
-      "padding": "40px 0",
-      "background-color": "transparent"
+  $(".header__wrap").mouseleave(function () {
+    $(".header__wrap").css({
+      padding: "40px 0",
+      "background-color": "transparent",
     });
-    $('.gnb__menu .gnb__item > a').css("color", "#fff");
-    $('.logo img').attr('src', "img/transparent__logo.svg")
+    $(".gnb__menu .gnb__item > a").css("color", "#fff");
+    $(".logo img").attr("src", "../img/transparent__logo.svg");
 
-
-    $('.sub__menu').css({
-      "display": "none",
-      "opacity": 0
+    $(".sub__menu").css({
+      display: "none",
+      opacity: 0,
     });
 
-    $('.sub__menu li a').css("display", "none");
+    $(".sub__menu li a").css("display", "none");
   });
-})
+});
 
 /**
- * 모바일 gnb 토글 
+ * 모바일 gnb 토글
  * --------------------------
  * 1. 모바일 햄버거 버튼 클릭 시 mobile-gnb display block 처리
  * 2. body 태그의 overflow 또한 토글 처리하면서 스크롤 막기
  * 3. 모바일 기준 width 100%, height을 100vh로 설정해 전체 영역 감싸기
- *  
- * 
+ *
+ *
  */
 
 // 현재 하고 싶은 것 : 첫번째 클릭 시 toggle 되면서 body 스크롤 막기, 두번째 클릭시 다시 visible 풀기
 
-$(document).ready(function(){
+$(document).ready(function () {
   // $('.mobile__gnb').hide();
-  $('.mobile--opener').click(function(){
-    $('.mobile__gnb').toggle();
-
-
-  })
-})
+  $(".mobile--opener").click(function () {
+    $(".mobile__gnb").toggle();
+  });
+});
 
 /**
  *  모바일 gnb 아코디언
@@ -122,77 +116,73 @@ $(document).ready(function(){
 
 let moBtnIndex;
 
-  $('.mo__item > a').click(function(){
-    moBtnIndex = $('.mo__item > a').index(this)
-    $('.mobile__gnb .mo__sub').eq(moBtnIndex).toggle();
-  })
-
-
-
-  /**
-   * con-box 마우스 호버 시 백그라운드 이미지 크기 확대
-   * 
-   */
-
-  let box = $('.con__box');
-  console.log(box)
-
-  $(document).ready(function() {
-    $('.con__box').hover(
-      function() {
-
-        conIndex = $('.con__box').index(this)
-        $(this).css({'background-size' : '150% 150%'});
-        // $(this).css('transform', 'scale(1.1)');
-        $('.con__box').eq(conIndex).css("z-index", "333")
-        $('.con__box').not($(this)).css({
-          "z-index" : "332",
-          "opacity" : "0.9",
-        })
-      },
-      function() {
-        $(this).css({'background-size' : '100% 100%'});
-        // $(this).css('transform', 'scale(1)');
-        $('.con__box').eq(conIndex).css("z-index", "333")
-        $('.con__box').not($(this)).css({
-          "opacity" : "1"
-        })
-      }
-    );
-  });
-
-
-  /**
-   * map 변경 버튼
-   * 
-   * 각 버튼 누르면 기존 창 비활성화 및 새로운 창 활성화
-   */
-
-
-  var map = $('.map')
-  console.log(map)
-
-  $(document).ready(function(){
-    $('.map img').eq(0).css('opacity', '1')
-    $('.map img').eq(1).css('opacity', '0')
-  })
-
-
-  $('.map__desc button').click(function(){
-    var mapBtnIndex = $('.map__desc button').index(this);
-    $('.map img').eq(mapBtnIndex).css("opacity", '1');
-    $('.map img').not($('.map img').eq(mapBtnIndex)).css("opacity", '0');
-  });
+$(".mo__item > a").click(function () {
+  moBtnIndex = $(".mo__item > a").index(this);
+  $(".mobile__gnb .mo__sub").eq(moBtnIndex).toggle();
+});
 
 /**
- * 
- * 모바일 content
- * 
- * 
- * 
+ * con-box 마우스 호버 시 백그라운드 이미지 크기 확대
+ *
  */
 
-$('.company__overview .overview__right .content a').mouseenter(function(){
-  $(this).addClass('active')
-  $('.company__overview .overview__right .content a').not($(this)).removeClass('active')
-})
+let box = $(".con__box");
+console.log(box);
+
+$(document).ready(function () {
+  $(".con__box").hover(
+    function () {
+      conIndex = $(".con__box").index(this);
+      $(this).css({ "background-size": "150% 150%" });
+      // $(this).css('transform', 'scale(1.1)');
+      $(".con__box").eq(conIndex).css("z-index", "333");
+      $(".con__box").not($(this)).css({
+        "z-index": "332",
+        opacity: "0.9",
+      });
+    },
+    function () {
+      $(this).css({ "background-size": "100% 100%" });
+      // $(this).css('transform', 'scale(1)');
+      $(".con__box").eq(conIndex).css("z-index", "333");
+      $(".con__box").not($(this)).css({
+        opacity: "1",
+      });
+    }
+  );
+});
+
+/**
+ * map 변경 버튼
+ *
+ * 각 버튼 누르면 기존 창 비활성화 및 새로운 창 활성화
+ */
+
+var map = $(".map");
+console.log(map);
+
+$(document).ready(function () {
+  $(".map img").eq(0).css("opacity", "1");
+  $(".map img").eq(1).css("opacity", "0");
+});
+
+$(".map__desc button").click(function () {
+  var mapBtnIndex = $(".map__desc button").index(this);
+  $(".map img").eq(mapBtnIndex).css("opacity", "1");
+  $(".map img").not($(".map img").eq(mapBtnIndex)).css("opacity", "0");
+});
+
+/**
+ *
+ * 모바일 content
+ *
+ *
+ *
+ */
+
+$(".company__overview .overview__right .content a").mouseenter(function () {
+  $(this).addClass("active");
+  $(".company__overview .overview__right .content a")
+    .not($(this))
+    .removeClass("active");
+});
